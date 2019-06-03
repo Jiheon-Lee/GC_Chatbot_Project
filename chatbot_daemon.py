676def6338c4ddff1df2d7b6480b4cede3781e35
mingyu <rlaalsrbgk@gmail.com>
2019-06-03 15:27:34 +0900

    start 명령어 추가 및 connect DB 추가

--- chatbot_daemon.py
+++ chatbot_daemon.py
@@ -2,9 +2,32 @@ import sys
 from models import *
 
 
+def connect_database():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+        secrets['RDS_USER_ID'],
+        secrets['RDS_USER_PASSWORD'],
+        secrets['RDS_USER_URL'],
+        secrets['RDS_PORT'],
+        secrets['RDS_DATABASE']
+    ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    return session
+
+
+def proc_start(bot, update):
+    chii.sendMessage("데이터베이스에 접속중입니다.")
+    session = connect_database()
+    chii.sendMessage("데이터베이스 접속 성공")
+    cctv = session.query(Cctv).all()
+    chii.sendMessage(cctv[0].address)
+
+
 def proc_restart(bot, update):
-    chii.sendMessage('다시 재 시작 합니다!!')
-    chii.start()
+    chii.sendMessage("재접속중.....")
+    connect_database()
+    chii.sendMessage("데이터베이스 접속 성공")
 
 
 def proc_rolling(bot, update):
@@ -15,8 +38,7 @@ def proc_rolling(bot, update):
 
 
 def proc_stop(bot, update):
-    chii.sendMessage('치이 봇이 잠듭니다.')
-    chii.stop()
+    chii.sendMessage("사용해 주셔서 감사합니다.")
 
 
 def proc_study(bot, update):
@@ -27,8 +49,13 @@ def firecracker():
     return '팡팡!'
 
 
+def cctv(bot, update):
+    chii.sendMessage("")
+
+
 chii = CctvBot()
 chii.add_handler('restart', proc_restart)
+chii.add_handler('start', proc_start)
 chii.add_handler('rolling', proc_rolling)
 chii.add_handler('stop', proc_stop)
 chii.add_handler('study', proc_study)
