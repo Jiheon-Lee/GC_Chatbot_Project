d73099d5bf282c9dc929d09162050dfda58dc4e9
mingyu <rlaalsrbgk@gmail.com>
2019-06-05 21:25:56 +0900

    DB connect

---
+++ connect_database.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_DIR = os.path.join(ROOT_DIR, '.secrets')
+secrets = json.load(open(os.path.join(SECRET_DIR, "secrets.json")))
+
+Base = declarative_base()
+
+
+def connect_database():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+        secrets['RDS_USER_ID'],
+        secrets['RDS_USER_PASSWORD'],
+        secrets['RDS_USER_URL'],
+        secrets['RDS_PORT'],
+        secrets['RDS_DATABASE']
+    ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    return session
